66b8caa8685f460b91c5b20ba1e843e54c5d2857
Matthew Simpson <matthew@matthewsimpson.net>
2023-05-05 14:56:22 +0100

    Handle missing files

--- fit/src/fit.rs
+++ fit/src/fit.rs
@@ -1,16 +1,29 @@
 use std::io::Read;
 use std::{fs::File, path::PathBuf};
 
+#[derive(Debug, PartialEq)]
 pub struct Fit {
     raw: Vec<u8>,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    FileNotFound,
+}
+
 impl Fit {
-    pub fn from_file(path: PathBuf) -> Option<Fit> {
-        let mut file = File::open(path).unwrap();
-        let mut raw = vec![];
-        file.read_to_end(&mut raw).ok()?;
-        Some(Fit { raw })
+    pub fn from_file(path: PathBuf) -> Result<Fit, Error> {
+        let file = File::open(path);
+        match file {
+            Ok(mut file) => {
+                let mut raw = vec![];
+                match file.read_to_end(&mut raw) {
+                    Ok(_) => Ok(Fit { raw }),
+                    Err(_) => Err(Error::FileNotFound),
+                }
+            }
+            Err(_) => Err(Error::FileNotFound),
+        }
     }
 }
 
@@ -21,7 +34,7 @@ mod tests {
     use std::{fs::File, path::PathBuf};
 
     #[test]
-    fn raw() {
+    fn file_can_be_loaded() {
         let path: PathBuf = PathBuf::from("../data/Activity.fit");
         let mut file = File::open("../data/Activity.fit").unwrap();
         let mut data = vec![];
@@ -30,5 +43,14 @@ mod tests {
         let activity = Fit::from_file(path).unwrap();
 
         assert_eq!(*activity.raw, data);
+    }
+
+    #[test]
+    fn missing_file_returns_error() {
+        let path: PathBuf = PathBuf::from("../data/Activity_missing.fit");
+        let activity = Fit::from_file(path);
+        let expected = Err(Error::FileNotFound);
+
+        assert_eq!(activity, expected);
     }
 }
