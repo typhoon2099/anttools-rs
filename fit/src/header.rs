0f0dd5bd84a187907c5d1a1218b589700735c0eb
Matthew Simpson <matthew@matthewsimpson.net>
2023-05-16 13:11:27 +0100

    Return None when ".FIT" isn't present

--- fit/src/header.rs
+++ fit/src/header.rs
@@ -1,3 +1,5 @@
+use std::str;
+
 pub struct Header {
     pub protocol_version: u8,
     pub profile_version: u16,
@@ -13,6 +15,11 @@ impl Header {
         }
 
         let header = &from[..header_size];
+
+        if str::from_utf8(&header[8..=11]) != Ok(".FIT") {
+            return None
+        }
+
         let protocol_version = *header.get(1).unwrap();
         let profile_version = u16::from_le_bytes([header[2], header[3]]);
         let data_size = u32::from_le_bytes([header[4], header[5], header[6], header[7]]);
@@ -30,6 +37,7 @@ mod tests {
     use super::*;
 
     const VALID_HEADER: [u8; 14] = [14, 32, 99, 8, 128, 111, 1, 0, 46, 70, 73, 84, 158, 67];
+    const NO_FIT: [u8; 14] = [14, 32, 99, 8, 128, 111, 1, 0, 0, 0, 0, 0, 158, 67];
 
     #[test]
     fn properties() {
@@ -43,6 +51,13 @@ mod tests {
     #[test]
     fn wrong_length() {
         let header = Header::from(&VALID_HEADER[0..11]);
+
+        assert!(header.is_none());
+    }
+
+    #[test]
+    fn no_fit_text() {
+        let header = Header::from(&NO_FIT);
 
         assert!(header.is_none());
     }
