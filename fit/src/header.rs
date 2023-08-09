afc27fa52007867fa7881af5b60b2ac5c907ac82
Matthew Simpson <matthew@matthewsimpson.net>
2023-08-09 21:04:48 +0100

    Return error when CRC is not valid

--- fit/src/header.rs
+++ fit/src/header.rs
@@ -4,6 +4,7 @@ use std::str;
 pub enum Error {
     WrongLength,
     FitTextMissing,
+    ChecksumFailed,
 }
 
 #[derive(Debug, PartialEq)]
@@ -18,13 +19,19 @@ impl Header {
         let header_size = *from.first().unwrap() as usize;
 
         if from.len() < header_size {
-            return Err(Error::WrongLength)
+            return Err(Error::WrongLength);
         }
 
         let header = &from[..header_size];
 
         if str::from_utf8(&header[8..=11]) != Ok(".FIT") {
-            return Err(Error::FitTextMissing)
+            return Err(Error::FitTextMissing);
+        }
+
+        let checksum = u16::from_le_bytes([from[12], from[13]]);
+
+        if checksum != 0 && checksum != Self::crc(&from[0..12]) {
+            return Err(Error::ChecksumFailed);
         }
 
         let protocol_version = *header.get(1).unwrap();
@@ -37,6 +44,26 @@ impl Header {
             data_size,
         })
     }
+
+    fn crc(from: &[u8]) -> u16 {
+        let mut crc: u16 = 0;
+        let crc_table: [u16; 16] = [
+            0x0000, 0xCC01, 0xD801, 0x1400, 0xF001, 0x3C00, 0x2800, 0xE401, 0xA001, 0x6C00, 0x7800,
+            0xB401, 0x5000, 0x9C01, 0x8801, 0x4400,
+        ];
+
+        for byte in from.iter() {
+            let tmp = crc_table[(crc & 0xF) as usize];
+            crc = (crc >> 4) & 0x0FFF;
+            crc = crc ^ tmp ^ crc_table[(byte & 0xF) as usize];
+
+            let tmp = crc_table[(crc & 0xF) as usize];
+            crc = (crc >> 4) & 0x0FFF;
+            crc = crc ^ tmp ^ crc_table[((byte >> 4) & 0xF) as usize];
+        }
+
+        crc
+    }
 }
 
 #[cfg(test)]
@@ -45,6 +72,8 @@ mod tests {
 
     const VALID_HEADER: [u8; 14] = [14, 32, 99, 8, 128, 111, 1, 0, 46, 70, 73, 84, 158, 67];
     const NO_FIT: [u8; 14] = [14, 32, 99, 8, 128, 111, 1, 0, 0, 0, 0, 0, 158, 67];
+    const INVALID_HEADER: [u8; 14] = [14, 32, 99, 8, 128, 111, 1, 0, 46, 70, 73, 84, 158, 66];
+    const NO_CRC: [u8; 14] = [14, 32, 99, 8, 128, 111, 1, 0, 46, 70, 73, 84, 0, 0];
 
     #[test]
     fn properties() {
@@ -67,5 +96,19 @@ mod tests {
         let result = Header::from(&NO_FIT);
 
         assert_eq!(result, Err(Error::FitTextMissing));
+    }
+
+    #[test]
+    fn invalid_crc() {
+        let result = Header::from(&INVALID_HEADER);
+
+        assert_eq!(result, Err(Error::ChecksumFailed));
+    }
+
+    #[test]
+    fn no_crc() {
+        let result = Header::from(&NO_CRC);
+
+        assert!(result.is_ok());
     }
 }
