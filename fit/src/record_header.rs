086ba8cb016534611f1fef0d5e6b2cae4b8848a1
Matthew Simpson <matthew@matthewsimpson.net>
2024-02-03 22:02:50 +0000

    Get rest of Normal Message header values

--- fit/src/record_header.rs
+++ fit/src/record_header.rs
@@ -14,12 +14,12 @@ pub struct RecordHeader {
 
 impl RecordHeader {
     pub fn from(from: &u8) -> RecordHeader {
-        let compressed = 0b10000000 & from == 0b10000000;
+        let compressed = 0b1000_0000 & from == 0b1000_0000;
 
         let message_type = match compressed {
             true => MessageType::CompressedTimestamp,
             false => {
-                let definition = 0b01000000 & from == 0b01000000;
+                let definition = 0b0100_0000 & from == 0b0100_0000;
 
                 match definition {
                     true => MessageType::Definition,
@@ -27,10 +27,14 @@ impl RecordHeader {
                 }
             }
         };
+
+        let message_type_specific = 0b0010_0000 & from == 0b0010_0000;
+        let local_message_type = 0b0000_1111 & from;
+
         RecordHeader {
             message_type,
-            message_type_specific: false,
-            local_message_type: 0,
+            message_type_specific,
+            local_message_type,
             time_offset: None,
         }
     }
@@ -52,6 +56,20 @@ mod tests {
         let header = RecordHeader::from(&0);
 
         assert_eq!(header.message_type, MessageType::Data);
+    }
+
+    #[test]
+    fn message_type_specific() {
+        let header = RecordHeader::from(&0b00100000);
+
+        assert!(header.message_type_specific);
+    }
+
+    #[test]
+    fn local_message_type() {
+        let header = RecordHeader::from(&0b01001111);
+
+        assert_eq!(header.local_message_type, 15);
     }
 
     #[test]
