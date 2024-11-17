834267fd7b28fe15f1250b57b7366d7c0e3be366
Matthew Simpson <matthew@matthewsimpson.net>
2024-11-17 21:59:32 +0000

    Only concern `Fit` with loading the file
    We'll write an impl Iter to iterate over the records in the file later.

--- fit/src/fit.rs
+++ fit/src/fit.rs
@@ -1,16 +1,12 @@
 use crate::crc;
 use crate::header::Header;
-use crate::record::Record;
-use crate::record_header::{MessageType, RecordHeader};
 use std::io::{BufReader, Read, Seek};
 use std::{fs::File, path::PathBuf};
 
-// TODO: Make records a new Struct
-#[derive(Debug, PartialEq)]
-pub struct Fit {
-    pub protocol_version: u8,
-    pub profile_version: u16,
-    pub records: Vec<Record>,
+#[derive(Debug)]
+pub struct Fit<'a> {
+    header: Header,
+    record_data: Option<&'a mut BufReader<File>>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -19,25 +15,25 @@ pub enum Error {
     FileNotValid,
 }
 
-impl Fit {
-    pub fn from_file(path: PathBuf) -> Result<Fit, Error> {
+impl<'a> Fit<'a> {
+    pub fn from_file(path: PathBuf) -> Result<Fit<'static>, Error> {
         let file = File::open(path);
         match file {
             Ok(file) => {
-                let mut buf = BufReader::new(file);
+                let buf = BufReader::new(file);
 
-                Fit::read(&mut buf)
+                Fit::read(buf)
             }
             Err(_) => Err(Error::FileNotFound),
         }
     }
 
-    fn read(from: &mut BufReader<File>) -> Result<Fit, Error> {
+    fn read(mut from: BufReader<File>) -> Result<Fit<'a>, Error> {
         let mut checksum_data = vec![];
         from.read_to_end(&mut checksum_data).unwrap();
         from.rewind().unwrap();
 
-        let header = match Header::from(from) {
+        let header = match Header::from(&mut from) {
             Ok(header) => header,
             Err(_error) => return Err(Error::FileNotValid),
         };
@@ -49,30 +45,9 @@ impl Fit {
             return Err(Error::FileNotValid);
         }
 
-        let mut records = vec![];
-
-        let mut record_header_byte = vec![0u8; 1];
-        let _ = from.read_exact(&mut record_header_byte);
-        let next_record_header = RecordHeader::from(record_header_byte[0]);
-
-        match next_record_header.message_type {
-            MessageType::Definition => {
-                records.push(Record::definition(from));
-            }
-            MessageType::Data => {
-                println!("Data");
-                println!("{}", from.stream_position().unwrap());
-            }
-            MessageType::CompressedTimestamp => {
-                println!("CompressedTimestamp");
-                println!("{}", from.stream_position().unwrap());
-            }
-        }
-
-        Ok(Fit {
-            protocol_version: header.protocol_version,
-            profile_version: header.profile_version,
-            records,
+        Ok(Self {
+            header,
+            record_data: None,
         })
     }
 }
@@ -85,44 +60,35 @@ mod tests {
     #[test]
     fn file_can_be_loaded() {
         let path: PathBuf = PathBuf::from("../data/Activity.fit");
-        let activity = Fit::from_file(path).unwrap();
+        let activity = Fit::from_file(path);
 
-        assert_eq!(activity.protocol_version, 32);
-        assert_eq!(activity.profile_version, 2147);
-    }
-
-    #[test]
-    fn stores_records() {
-        let path: PathBuf = PathBuf::from("../data/Activity.fit");
-        let activity = Fit::from_file(path).unwrap();
-
-        assert_eq!(activity.records.len(), 94080);
+        assert!(activity.is_ok());
     }
 
     #[test]
     fn missing_file_returns_error() {
         let path: PathBuf = PathBuf::from("../data/Activity_missing.fit");
         let activity = Fit::from_file(path);
-        let expected = Err(Error::FileNotFound);
+        let expected = Error::FileNotFound;
 
-        assert_eq!(activity, expected);
+        assert_eq!(activity.unwrap_err(), expected);
     }
 
     #[test]
     fn invalid_fit_file_returns_error() {
         let path: PathBuf = PathBuf::from("../data/Activity_invalid_header.fit");
         let activity = Fit::from_file(path);
-        let expected = Err(Error::FileNotValid);
+        let expected = Error::FileNotValid;
 
-        assert_eq!(activity, expected);
+        assert_eq!(activity.unwrap_err(), expected);
     }
 
     #[test]
     fn invalid_fit_data_returns_error() {
         let path: PathBuf = PathBuf::from("../data/Activity_invalid_data.fit");
         let activity = Fit::from_file(path);
-        let expected = Err(Error::FileNotValid);
+        let expected = Error::FileNotValid;
 
-        assert_eq!(activity, expected);
+        assert_eq!(activity.unwrap_err(), expected);
     }
 }
